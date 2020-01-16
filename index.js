6b4d65f0158734f1ec9f14f293ee7bf350713558
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-16 09:14:58 -0800

    add in async fetch request.

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ app.listen(3000, () => console.log("listening at 3000"));
 
 //serve static files with middleware
 app.use(express.static("public"));
+
+//Recognize incoming request object as JSON
+app.use(express.json({ limit: "1mb" }));
+
+//post request. Callback looks at info- sends response back
+//request holds all of the information about the particular client
+//response can send things back to the client
+app.post("/api", (request, response) => {
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+    latitude: data.lat,
+    longitude: data.lon
+  });
+});
